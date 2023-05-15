c374d1f224f0bd1dba8952a253725f828bd7325f
Ahsan Raza <mahsanr44@gmail.com>
2023-05-16 02:07:48 +0500

    Backend Implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Footer from './Footer';
 import Weather from './Weather';
 
 function App() {
   return (
     <>
-    <div className='bg-gray-800 max-w-full p-10'>
+    <div className='bg-gray-800 max-w-full pt-6'>
 
     <Weather/>
+    <Footer/>
     </div>
     </>
   );
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-200 text-cyan-700 mt-72 p-2 text-center font-semibold">
+      Designed with ❤️ By Ahsan
+    </div>
+  );
+};
+
+export default Footer;
